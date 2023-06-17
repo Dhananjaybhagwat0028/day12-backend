abec6178b3cfbd96f87e059de92f0b81580077b2
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-18 03:20:07 +0530

    UPDATED

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import cors from "cors";
 const app = express();
 app.use(cors()); // allowing everyone.
 
+// required in post
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 async function addrecord(req, res) {
   const uri = "mongodb://127.0.0.1:27017";
   const client = new MongoClient(uri);
@@ -46,67 +50,135 @@ function helloPost(req, res) {
 
 // NEW TODO API
 async function addTodo(req, res) {
-  const uri = "mongodb://127.0.0.1:27017";
-  const client = new MongoClient(uri);
+  try {
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
 
-  const db = client.db("project");
-  const messageColl = db.collection("todo");
+    const db = client.db("project");
+    const messageColl = db.collection("todo");
 
-  let inputDoc = {
-    task: req.query.task,
-    description: req.query.description,
-  };
-  await messageColl.insertOne(inputDoc);
+    let inputDoc = {
+      task: req.query.task,
+      description: req.query.description,
+    };
+    await messageColl.insertOne(inputDoc);
 
-  await client.close();
-
-  res.json({ opr: "success" });
+    await client.close();
+    res.json({ opr: true });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 async function findAllTodo(req, res) {
-  const uri = "mongodb://127.0.0.1:27017";
-  const client = new MongoClient(uri);
+  try {
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
 
-  const db = client.db("project");
-  const messageColl = db.collection("todo");
+    const db = client.db("project");
+    const messageColl = db.collection("todo");
 
-  let list = await messageColl.find().toArray();
+    let list = await messageColl.find().toArray();
 
-  await client.close();
-  res.json(list);
+    await client.close();
+    res.json(list);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 async function addUserRecord(req, res) {
-  const uri = "mongodb://127.0.0.1:27017";
-  const client = new MongoClient(uri);
+  try {
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
 
-  const db = client.db("project");
-  const messageColl = db.collection("user");
+    const db = client.db("project");
+    const messageColl = db.collection("user");
 
-  let inputDoc = {
-    username: req.query.username,
-    password: req.query.password,
-    email: req.query.email,
-    mobile: req.query.mobile,
-  };
-  await messageColl.insertOne(inputDoc);
+    let inputDoc = {
+      username: req.query.username,
+      password: req.query.password,
+      email: req.query.email,
+      mobile: req.query.mobile,
+    };
+    await messageColl.insertOne(inputDoc);
 
-  await client.close();
+    await client.close();
 
-  res.json({ opr: "success" });
+    res.json({ opr: true });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 async function findAllUser(req, res) {
-  const uri = "mongodb://127.0.0.1:27017";
-  const client = new MongoClient(uri);
+  try {
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
 
-  const db = client.db("project");
-  const messageColl = db.collection("user");
+    const db = client.db("project");
+    const messageColl = db.collection("user");
 
-  let list = await messageColl.find().toArray();
+    let list = await messageColl.find().toArray();
 
-  await client.close();
-  res.json(list);
+    await client.close();
+    res.json(list);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+// LOGIN - AUTHENTICATION
+async function loginByGet(req, res) {
+  try {
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
+
+    const db = client.db("project");
+    const messageColl = db.collection("user");
+
+    let query = { email: req.query.email, password: req.query.password };
+    let userRef = await messageColl.findOne(query);
+
+    await client.close();
+
+    // Negative: UserRef CANBE NULL;
+    if (!userRef) {
+      let errorMessage = `Record Not Found or Authentication Failure: ${req.query.email}`;
+      throw new Error(errorMessage);
+    }
+
+    // Postive Scenario
+    res.json(userRef);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+async function loginByPost(req, res) {
+  try {
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
+
+    const db = client.db("project");
+    const messageColl = db.collection("user");
+
+    let query = { email: req.body.email, password: req.body.password };
+    let userRef = await messageColl.findOne(query);
+
+    await client.close();
+
+    // Negative: UserRef CANBE NULL;
+    if (!userRef) {
+      let errorMessage = `Record Not Found or Authentication Failure: ${req.body.email}`;
+      throw new Error(errorMessage);
+    }
+
+    // Postive Scenario
+    res.json(userRef);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 // http://localhost:4000/addrecord
@@ -117,6 +189,8 @@ app.get("/addtodo", addTodo);
 app.get("/find-all-todo", findAllTodo);
 app.get("/adduser", addUserRecord);
 app.get("/find-all-user", findAllUser);
+app.get("/login-by-get", loginByGet);
+app.post("/login-by-post", loginByPost);
 
 // http://localhost:4000/
 app.listen(4000);
